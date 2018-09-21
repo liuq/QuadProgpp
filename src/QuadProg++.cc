fe5fde412b9f5825e071a8dc63cd84b79d5ea2b8
Henrik Lindberg <563066+hlindberg@users.noreply.github.com>
2018-09-21 12:07:10 +0200

    Drop unused but set variable 'q'
    The variable q is initialized with a 0 and set to various values
    but it is never read. This commit removes the variable 'q'.

--- src/QuadProg++.cc
+++ src/QuadProg++.cc
@@ -95,12 +95,11 @@ double solve_quadprog(Matrix<double>& G, Vector<double>& g0,
   double t, t1, t2; /* t is the step lenght, which is the minimum of the partial step length t1 
     * and the full step length t2 */
   Vector<int> A(m + p), A_old(m + p), iai(m + p);
-  unsigned int q, iq, iter = 0;
+  unsigned int iq, iter = 0;
   Vector<bool> iaexcl(m + p);
 	
   /* p is the number of equality constraints */
   /* m is the number of inequality constraints */
-  q = 0;  /* size of the active set A (containing the indices of the active constraints) */
 #ifdef TRACE_SOLVER
   std::cout << std::endl << "Starting solve_quadprog" << std::endl;
   print_matrix("G", G);
@@ -247,7 +246,7 @@ l1:	iter++;
   if (fabs(psi) <= m * std::numeric_limits<double>::epsilon() * c1 * c2* 100.0)
   {
     /* numerically there are not infeasibilities anymore */
-    q = iq;
+//    q = iq;
     
     return f_value;
   }
@@ -273,7 +272,7 @@ l2: /* Step 2: check for feasibility and determine a new S-pair */
     }
   if (ss >= 0.0)
   {
-    q = iq;
+//    q = iq;
     
     return f_value;
   }
@@ -345,7 +344,6 @@ l2a:/* Step 2a: determine step direction */
   {
     /* QPP is infeasible */
     // FIXME: unbounded to raise
-    q = iq;
     return inf;
   }
   /* case (ii): step in dual space */
